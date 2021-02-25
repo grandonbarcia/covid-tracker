950ed9b69cde5671ca0873a339a965e50fb04615
Brandon Garcia <grandonleebarcia@gmail.com>
2021-02-25 12:44:32 -0800

    attempt to optimize using one component instead of three seperate ones to display global data

--- src/GlobalSummary/TotalPanel.js
+++ src/GlobalSummary/TotalPanel.js
@@ -1,44 +1,45 @@
-import React, {useContext} from 'react';
+import React, { useEffect, useContext } from 'react';
 import Country from "./Country";
 import Total from "./Total";
-import {DataContext} from "../DataProvider"
+import { DataContext } from "../DataProvider"
 import '../App.css';
 
 
-const TotalPanel = () => {
+const TotalPanel = ({ category, total, countries }) => {
 
   const { value, value4 } = useContext(DataContext);
 
   var [num] = value;
-  var [total] = value4;
 
 
 
-  
 
+  const listCountries = countries.map((data, i) =>
 
-  const listCountries = num.map((data, i) =>
-    <div className = "col-md-12" key={i}>
-      <Country  name={data.Country} num = {data.TotalConfirmed}   />
+    <div className="col-md-12" key={i}>
+      <Country name={data.Country} num={data.total} />
     </div>
   );
 
+  useEffect(() => {
+    console.log(category);
+    console.log(total);
+    console.log(countries);
+  }, [])
 
 
-  return(
-    <div className = "col-md-4">
-      <div className = "row justify-content-center">
-        Total Confirmed:
-        <div className = "col-md-12 title-name">
 
-          <Total num = {total.TotalConfirmed} />
+  return (
+    <div className="col-md-4">
+      <div className="row justify-content-center">
+        Total {category}:
+        <div className="col-md-12 title-name">
+          <Total total={total} />
         </div>
-
       </div>
-      <div className = "row justify-content-center">
+      <div className="row justify-content-center">
         {listCountries}
       </div>
-
     </div>
 
   );
